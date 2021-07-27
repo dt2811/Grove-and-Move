5b4b4d2d1d7c7e3a892161fb60e913bd754694e8
dhrumil <dhrumilthakore@gmail.com>
2021-07-27 16:29:31 +0530

    Removed profile page

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:grove_and_move/Screens/LandingPage.dart';
-import 'package:grove_and_move/Screens/MusicScreen.dart';
 import 'package:grove_and_move/Screens/PartyPage.dart';
-import 'package:grove_and_move/Screens/PlayList.dart';
 import 'package:grove_and_move/Screens/SearchScreen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -19,16 +17,12 @@ class _HomeScreen extends State<HomeScreen> {
     LandingPage(),
     SearchScreen(),
     PartyPage(),
-    Container(
-      color: Colors.white,
-    )
   ];
 
   int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    final _scaffoldKey = new GlobalKey<ScaffoldState>();
     return Scaffold(
       body: tabs[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
@@ -48,10 +42,6 @@ class _HomeScreen extends State<HomeScreen> {
           BottomNavigationBarItem(
               icon: Icon(Icons.blur_on_outlined),
               label: "Join",
-              backgroundColor: Colors.blueGrey[900]),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.account_box),
-              label: "Profile",
               backgroundColor: Colors.blueGrey[900]),
         ],
         onTap: (index) {
